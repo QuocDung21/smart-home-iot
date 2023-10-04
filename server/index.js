69f85644b8c6cd88e59250679f7aafe56d36c4d3
QuocDung21 <rongvang2357hn@gmail.com>
2023-10-04 14:20:25 +0700

    add

--- server/index.js
+++ server/index.js
@@ -28,9 +28,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/", (req, res) => {
-  return res.json("Api running...");
-});
+            app.use("/", (req, res) => {
+            return res.json("Api running...");
+            });
 
 app.use("/api/house", houseRouter);
 app.use("/api/auth", authRouter);
